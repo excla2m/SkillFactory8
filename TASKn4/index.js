8bb26a21f6fb41d69cc3de3d8badd7fef4616c19
Egor <egbeket@gmail.com>
2021-02-14 16:35:04 +0300

    git

--- TASKn4/index.js
+++ TASKn4/index.js
@@ -2,4 +2,4 @@
 let x = Math.random(0,1);
 let y = x * 100;
 let z = Math.round(y)
-console.log(z)
+alert(z) 
---
+++ TASKn5/index.js
@@ -0,0 +1,5 @@
+let array = [4, 8, 15, 67, 50, 89]
+console.log(array.length);
+for (let i = 0; i < array.length; i++) { 
+    console.log(array[i]);
+  }
