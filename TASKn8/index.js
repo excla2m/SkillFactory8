206bf56fc90a7e638f4ad88cbd08eecce33b6167
Egor <egbeket@gmail.com>
2021-02-15 16:57:21 +0300

    8th task

---
+++ TASKn8/index.js
@@ -0,0 +1,8 @@
+let fruits = new Map();
+fruits.set(1, "apple"); 
+fruits.set(2, "banana"); 
+fruits.set(3, "orange"); 
+alert(fruits.size); 
+for (let elem of fruits) { 
+    console.log(elem);
+ }
